35eb5dfc7827d83eede6d7515a9664b420e25150
John Watts <johnwatts@gmx.com>
2017-05-03 15:58:54 -0500

    Edit show users posts to show all posts by that user in same format as all posts

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
-<h1>{{ $user->name }}</h1>
+<h1 style="text-align: center; margin-bottom: 5%;">{{ $user->name }}</h1>
+
+<div class="container">
+	<div class="row" id="itemsPage">
+		@foreach($posts as $post)
+			<div class="col-sm-4" >
+					<div class="panel panel-primary" >
+						<div class="panel-heading" id="itemsPanelColor">
+							<a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></div>
+						<div class="panel-body">{{ substr($post->content, 0, 40) . "..." }}</div>
+						<div class="panel-body">{{ substr($post->url, 0, 40) . "..." }}</div>
+						<div class="panel-footer">Posted by: <strong>{{ $post->user->name }}
+							</strong> on: <strong>{{ $post->created_at->setTimezone('America/Chicago')->toDayDateTimeString() }}
+							</strong>
+						</div>
+					</div>
+				</a>
+			</div>
+		@endforeach
+
+{!! $posts->render() !!}
 
 @stop
