0f0d964765c9ee23feaaf8792638a91063960fce
John Watts <johnwatts@gmx.com>
2017-05-03 12:29:51 -0500

    Changed layout by adding panels and updated CSS

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,35 +2,22 @@
 
 @section('content')
 
-		<h1>Posts</h1>
+		<h1 style="text-align: center; margin-bottom: 5%;">Posts</h1>
 
+		<div class="container">
+			<div class="row" id="itemsPage">
+				@foreach($posts as $post)
+					<div class="col-sm-4" >
+							<div class="panel panel-primary" >
+								<div class="panel-heading" id="itemsPanelColor"><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></div>
+								<div class="panel-body">{{ $post->content }}</div>
+								<div class="panel-footer">{{ $post->url }}</div>
+								<div class="panel-footer">Posted by: <strong>{{ $post->user->name }}</strong> on: <strong>{{ $post->created_at->setTimezone('America/Chicago')->toDayDateTimeString() }}</strong></div>
+							</div>
+						</a>
+					</div>
+				@endforeach
 
-		        <table class="table table-bordered table-hover table-responsive">
-		            <thead>
-						<tr>
-							<th>Title</th>
-							<th>Content</th>
-							<th>URL</th>
-							<th>Posted by</th>
-							<th>Posted</th>
-							<th>Updated</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($posts as $post)
-					   <tr>
-						 <td><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></td>
-						 <td>{{ $post->content }}</td>
-						 <td>{{ $post->url }}</td>
-						 <td>{{ $post->user->name }}</td>
-						 <td>{{ $post->created_at->setTimezone('America/Chicago')->toDayDateTimeString() }}</td>
-						 @if($post->created_at != $post->updated_at)
-						 <td>{{ $post->updated_at->setTimezone('America/Chicago')->diffForHumans() }}</td>
-						 @endif
-					 </tr>
-					 @endforeach
-				 </tbody>
-		        </table>
 
 
 		    {!! $posts->render() !!}
