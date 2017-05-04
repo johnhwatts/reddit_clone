0ca7f1d634ebed7c19bae392245e1a9316be25b0
John Watts <johnwatts@gmx.com>
2017-05-04 16:41:47 -0500

    successfully debugged, pushing changes made to my posts page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,21 +8,15 @@
 <div class="container">
 	<div class="row" id="itemsPage">
 		@foreach($user->posts as $post)
-			<div class="col-sm-4" >
-					<div class="panel panel-primary" >
-						<div class="panel-heading" id="itemsPanelColor">
-							<a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></div>
-						<div class="panel-body">{{ $post->content }}</div>
-						<div class="panel-body"><a href = "{{ ($post->url) }}">{{ $post->url }}</a></div>
-						<div class="panel-footer">Posted by: <strong>{{ $post->user->name }}
-							</strong> on: <strong>{{ $post->created_at->setTimezone('America/Chicago')->toDayDateTimeString() }}
-							</strong>
-						</div>
-					</div>
-				</a>
-			</div>
-
+		<article class= "col-md-4">
+			<h3><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></h3>
+			<p>{{ substr($post->url, 0, 40) . "..." }}</p>
+			<p>{{ substr($post->content, 0, 40) . "..." }}</p>
+			<p>Posted by: <strong>{{ $post->user->name }}</strong></p>
+			<p>on: <strong>{{ $post->created_at->setTimezone('America/Chicago')->toDayDateTimeString() }}</strong></p>
+		</article>
 		@endforeach
-
+	</div>
+</div>
 
 @stop
